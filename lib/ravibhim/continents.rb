d3769d1257d4d15c60d98b427f7afb51a7ac6cbf
Ravi Bhim <rbhimavarapu@attinteractive.com>
2010-07-06 16:28:45 -0700

    Basic Proof of Concept

---
+++ lib/ravibhim/continents.rb
@@ -0,0 +1,27 @@
+require 'yaml'
+
+module Ravibhim
+  module Continents
+    CONTINENTS = ['Asia', 'Australia']
+
+    # Load countries from yaml files.
+    # and build up country to continent mapping
+    COUNTRY_TO_CONTINENT = {}
+    CONTINENTS.each do |cont|
+      cont_file_name = cont.downcase.gsub(' ','_')
+      cont_const_name = cont.upcase.gsub(' ','_')
+
+      countries = YAML.load_file(File.dirname(__FILE__) + "/data/#{cont_file_name}.yml")
+      countries.each do |country|
+        COUNTRY_TO_CONTINENT[country] = cont
+      end
+
+      const_set("#{cont_const_name}_COUNTRIES", countries)
+    end
+
+
+    def self.get_continent(country)
+      COUNTRY_TO_CONTINENT[country]
+    end
+  end
+end
--- spec/continents_spec.rb
+++ spec/continents_spec.rb
@@ -1,7 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Continents" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+
+  describe "CONTINENTS" do
+    it 'gets set to the list of continents' do
+      %w(Asia Australia).each do |c|
+        Ravibhim::Continents::CONTINENTS.should include(c)
+      end
+    end
   end
+
+  describe "Asia Countries" do
+    ['India', 'Sri Lanka'].each do |c|
+      it "has #{c} in Asian countries" do
+        Ravibhim::Continents::ASIA_COUNTRIES.should include(c)
+      end
+    end
+  end
+
+  describe "Australia Countries" do
+    ['Australia', 'New Zealand'].each do |c|
+      it "has #{c} in Asian countries" do
+        Ravibhim::Continents::AUSTRALIA_COUNTRIES.should include(c)
+      end
+    end
+  end
+
+  describe :get_country do
+    it 'returns the continent given the country' do
+      Ravibhim::Continents::get_continent('India').should == 'Asia'
+    end
+
+    it 'returns nil for an unrecognized country' do
+      Ravibhim::Continents::get_continent('My Own Country').should == nil
+    end
+  end
+
 end
